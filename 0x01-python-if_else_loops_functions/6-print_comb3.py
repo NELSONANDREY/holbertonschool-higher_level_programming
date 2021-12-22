502f6ee9aeed4fe388902d4b84a0b58eb634e890
root <root@NELSON-G.localdomain>
2021-12-21 23:46:41 -0500

    print_comb

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for nu in range(0, 10):
-    for nu2 in range(0, 10):
-        if nu is 8 and nu2 is 9:
+for nu in range(0, 9):
+    for nu2 in range(nu + 1, 10):
+        if nu == 8:
             print("{:d}{:d}".format(nu, nu2))
-        elif nu < nu2:
+        else:
             print("{:d}{:d}".format(nu, nu2), end=", ")
