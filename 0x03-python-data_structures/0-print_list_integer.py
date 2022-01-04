eea64c9e7eac68170492be5fa0c8b8080716cacd
NELSONANDREY <3424@holbertonschool.com>
2022-01-04 11:11:42 -0500

    element_at

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for iterador in range(len(my_list)):
-        print("{:d}".format(my_list[iterador]))
+    for i in my_list:
+        print("{:d}".format(my_list[i - 1]))
