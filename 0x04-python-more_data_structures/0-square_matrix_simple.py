85c118dd4215efb8f2e30b6b7a8bcdf65afd7edc
NELSONANDREY <3424@holbertonschool.com>
2022-01-05 16:57:07 -0500

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    if matrix:
-        for i in matrix:
-            fila = []
-            for k in i:
-                fila.append(k * k)
-            new_matrix.append(fila)
-    return new_matrix
+    if not matrix:
+        print()
+    return [[item**2 for item in row] for row in matrix]
