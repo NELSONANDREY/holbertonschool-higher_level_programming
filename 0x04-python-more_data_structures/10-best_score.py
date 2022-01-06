73c0009815c176b85a903238474dcf2e8652a717
NELSONANDREY <3424@holbertonschool.com>
2022-01-05 20:50:25 -0500

    corection folders2

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    laNueva = []
-    if matrix:
-        for iteradorMatrix in matrix:
-            fila = []
-            for iteradorContenido in iteradorMatrix:
-                fila.append(iteradorContenido * iteradorContenido)
-            laNueva.append(fila)
-        return (laNueva)
+def best_score(a_dictionary):
+    if a_dictionary:
+        val = 0
+        key = ""
+        for i in a_dictionary:
+            if a_dictionary[i]:
+                if a_dictionary[i] > val:
+                    val = a_dictionary[i]
+                    key = i
+        if key not in a_dictionary:
+            return None
+        else:
+            return key
