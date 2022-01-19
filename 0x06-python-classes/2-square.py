4361ae86402d3ff9ad1b10dfe7a9cdd7f464b60a
NELSONANDREY <3424@holbertonschool.com>
2022-01-18 21:06:14 -0500

    write a class square1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,11 @@
 
 
 class Square:
+    """class empty"""
     def __init__(self, size=0):
+        """generate atribute private
+        self: """
+        
         if type(size) != int:
             raise TypeError('size must be an integer')
         elif size < 0:
