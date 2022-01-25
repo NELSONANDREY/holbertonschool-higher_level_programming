9ce2347c237c382bb3c2ba444b092fc05a38af0d
NELSONANDREY <3424@holbertonschool.com>
2022-01-24 21:36:16 -0500

    area rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,26 +4,13 @@
 """
 
 
-class Rectangle:
+class Rectangle():
     """
     Rectangle functions and date
     """
     def __init__(self, width=0, height=0):
-        self.height = height
         self.width = width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__height = value
+        self.height = height
 
     @property
     def width(self):
@@ -37,3 +24,18 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+
