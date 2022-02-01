31796591a009793f08c48b00ddb6a8ff93e02ce8
NELSONANDREY <3424@holbertonschool.com>
2022-01-31 20:26:17 -0500

    every folders

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+"""MyList
+"""
+
+
+
+class Mylist(list):
+    """contains list
+    """
+
+    def print_sorted(self)
+    """Prints self in sorted format
+    """
+
+    print(sorted(self))
