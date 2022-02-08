fac6616d2cea425292a34b9f5515e70cfbab8922
NELSONANDREY <3424@holbertonschool.com>
2022-02-07 20:40:48 -0500

    every folders16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,11 +7,13 @@ import json
 
 
 class Base:
-    """Base of the other shapes
-    """
+    """class base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+
+        """initialize instance attributes"""
+
         if id is not None:
             self.id = id
         else:
@@ -20,44 +22,51 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ adding the static method"""
+        """return json string representation of list_dictionaries"""
         if list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
 
-    @staticmethod
-    def from_json_string(json_string):
-        """Returns JSON strings in list
-        """
-
-        if type(json_string) != str or len(json_string) == 0:
-            return []
-        return json.loads(json_string)
-
     @classmethod
     def save_to_file(cls, list_objs):
-        """Writes to file with JSON string
-        """
-
-        with open(cls.__name__ + ".json", mode="w") as write_file:
+        """writes the json string representation of list_objs  to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w") as file:
             if list_objs is None:
-                write_file.write("[]")
+                file.write("[]")
             else:
-                write_file.write(cls.to_json_string(
+                file.write(cls.to_json_string(
                         [element.to_dictionary() for element in list_objs]))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """return the list of the json string representing json_string"""
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """pendiente comentario"""
+        """return an instance with all attributes already set"""
         if cls.__name__ == "Rectangle":
-            clastmp = cls(1, 1)
+            dummy = cls(9, 8)
         if cls.__name__ == "Square":
-            clastmp == cls(1)
-        clastmp.update(**dictionary)
-        return clastmp
+            dummy = cls(9)
+        dummy.update(**dictionary)
+        return dummy
 
-    """ def load_from_file(cls):
-        pendiente comentario
-                with open(cls.__name__ + ".json", mode="r") as read_file:
-    """
+    @classmethod
+    def load_from_file(cls):
+        """read file in json format and return a list of instances"""
+        try:
+            with open(cls.__name__ + ".json", "r") as myfile:
+                to_read = myfile.read()
+                lists_dic = cls.from_json_string(to_read)
+                lists = []
+                for k in lists_dic:
+                    lists.append(cls.create(**k))
+                return lists
+        except Exception:
+            return []
