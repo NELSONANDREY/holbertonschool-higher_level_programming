1a819b55821db0bc08170f56d84123f564868c09
NELSONANDREY <3424@holbertonschool.com>
2022-04-12 11:47:10 -0500

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
-const request = require('request');
-request('http://www.google.com', function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, contents) {
+  if (!err) {
+    console.log(contents);
+  } else {
+    console.log(err);
+  }
 });
