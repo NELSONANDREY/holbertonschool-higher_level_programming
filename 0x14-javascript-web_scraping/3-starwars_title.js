76fbf90c9153970e2d3e1bd8265aee819fe7a814
NELSONANDREY <3424@holbertonschool.com>
2022-04-13 13:55:59 -0500

    starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
-require('request').get('http://swapi.co/api/films/' + process.argv[2] + '/', function (err, r, body) {
+const request = require('request');
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, response, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     console.log(JSON.parse(body).title);
   }
