c7d1c4744ae9c26ca67cda2b0a17529a1259bf37
NELSONANDREY <3424@holbertonschool.com>
2022-05-11 20:42:06 -0500

    every folders2

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
 """
 contains the class City
 """
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
